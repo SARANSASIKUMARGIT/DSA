b407953d1fa45be68fce08949378665da5bd09a3
Saran_SK <82465280+SARANSASIKUMARGIT@users.noreply.github.com>
2026-08-14 22:14:37 +0530

    Update Lowest-Common-Ancestor-BST.c
    C Program to calculate the Lowest Common Ancestor of Two Nodes .
    The Lowest Common Ancestor(LCA) is the deepest node in a tree that is the ancestor (top node of both the nodes),
    A Node can be an Ancestor to itself.
    In this program the LCA of two nodes is found by recursive approach
    
    THIS PROGRAM PERFORMS FINDING OF LCA ASSUMING THAT BOTH NODES P & Q ARE EXISTING IN THE NODE

--- BinarySearchTree/Lowest-Common-Ancestor-BST.c
+++ BinarySearchTree/Lowest-Common-Ancestor-BST.c
@@ -6,7 +6,11 @@
 /* Saran SK */
 
 /*
-    C Program to calculate the Lowest Common Ancestor of Two Nodes 
+    C Program to calculate the Lowest Common Ancestor of Two Nodes .
+    The Lowest Common Ancestor(LCA) is the deepest node in a tree that is the ancestor (top nodeof both the nodes),
+    A Node can be an Ancestor to itself.
+    In this program the LCA of two nodes is found by recursive approach
+    THIS PROGRAM PERFORMS FINDING OF LCA ASSUMING THAT BOTH NODES P & Q ARE EXISTING IN THE NODE
 */
 
 struct BSTNode *root=NULL;   //Pointer that stores the memory address of the root node of the binary search tree (initialy NULL)
@@ -44,11 +48,11 @@ struct BSTNode* insertNode(struct BSTNode* root,int data)      //Function that i
 
 struct BSTNode* LowestCommonAncestor(struct BSTNode* root,int p,int q)                      //function to find the LCA
 {
-    if(root==NULL)
+    if(root==NULL)                        //if node is null
         return NULL;
-    if(p< root->data && q< root->data)
+    if(p< root->data && q< root->data)    //if both the nodes are less than the current node , then it should be on the left subtree, therefore moving to leftSubtree by recursion
         return LowestCommonAncestor(root->left,p,q);
-    if(p> root->data && q > root->data )
+    if(p> root->data && q > root->data )    //if both the node are greater than the current node, then it should be on the right subtree, therefore moving to right Subtree by recursion
         return LowestCommonAncestor(root->right,p,q);
     return root;                    //Split point or one node is equal to the root
 }
@@ -73,7 +77,7 @@ int main()
     scanf("%d",&q);
     
     struct BSTNode* LCA = LowestCommonAncestor(root,p,q);                               //Calling Lowest Common Ancestor()
-    printf("LCA of Node %d and %d is %d \n",p,q,((LCA==NULL)?(-1):(LCA->data)));        //Printing the result 
+    printf("LCA of Node %d and %d is %d \n",p,q,((LCA==NULL)?(-1):(LCA->data)));        //Printing the result . if NULL , then it meaans No LCA is found (-1)
 
     return 0;
 }
