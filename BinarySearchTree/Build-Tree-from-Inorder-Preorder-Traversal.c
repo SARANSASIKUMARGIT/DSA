5638b58d435fef49c1b49926641d47fcbcdeb523
Saran_SK <82465280+SARANSASIKUMARGIT@users.noreply.github.com>
2026-08-16 23:45:54 +0530

    Update Build-Tree-from-Inorder-Preorder-Traversal.c
    C Program to construct a Binary Tree from Preorder + Inorder Traversal Array,
        Preorder Traversal = Root Node -> Left Node -> Right Node,
        Inorder  Traversal = Left Node -> Root Node -> Right Node,
        In the Inorder Array , all the elements(node) to the left of the root node are the nodes of the left Subtree of root node AND to the right elements are node of right Subtree,
        In the Preorder Array , always the first element is the root element, followed by all left subtrees then all it's right Subtree nodes.
    
        Approach : 
            *The First Element of the Preorder Array is the root node ,
            *Find the root node in the Inorder Array (rootNodeindex) ,
            *Count the number of elements left to the root node in Inorder array = LN
            *Count the number of elements right to the root node in Inorder array = RN
            *Create Binary Node with Value of Element InorderArray[rootNodeindex] 
            *Node -> left = ConstructTree(preArray , preStart,preEnd, inArray, inStart,inEnd)
            *Node ->right = ConstructTree(preArray, preStart,preEnd, inArray, inStart,inEnd)
    
            NOTE : THIS EXPLANATION MAY FEEL AMBIGIOUS BUT IT WILL BE UNDERSTOOD AFTER PRACTICAL / DRY RUN EXAMPLE BY YOURSELF
            "If There are N1 number of elements to the left of root element in Inorder array, then in the Preorder array after the root element the first N1 elements are left subtree nodes,
            and if there are N2 number of elements to the right of the root element in the Inorder Array, then there are N2 number of right subtree nodes after the N1 number of left subtrees node in the Preorder Array"

--- BinarySearchTree/Build-Tree-from-Inorder-Preorder-Traversal.c
+++ BinarySearchTree/Build-Tree-from-Inorder-Preorder-Traversal.c
@@ -6,7 +6,24 @@
 /* Saran SK */
 
 /*
-    C Program to construct a Binary Tree from Preorder + Inorder Traversal Array
+    C Program to construct a Binary Tree from Preorder + Inorder Traversal Array,
+    Preorder Traversal = Root Node -> Left Node -> Right Node,
+    Inorder  Traversal = Left Node -> Root Node -> Right Node,
+    In the Inorder Array , all the elements(node) to the left of the root node are the nodes of the left Subtree of root node AND to the right elements are node of right Subtree,
+    In the Preorder Array , always the first element is the root element, followed by all left subtrees then all it's right Subtree nodes.
+
+    Approach : 
+        *The First Element of the Preorder Array is the root node ,
+        *Find the root node in the Inorder Array (rootNodeindex) ,
+        *Count the number of elements left to the root node in Inorder array = LN
+        *Count the number of elements right to the root node in Inorder array = RN
+        *Create Binary Node with Value of Element InorderArray[rootNodeindex] 
+        *Node -> left = ConstructTree(preArray , preStart,preEnd, inArray, inStart,inEnd)
+        *Node ->right = ConstructTree(preArray, preStart,preEnd, inArray, inStart,inEnd)
+
+        NOTE : THIS EXPLANATION MAY FEEL AMBIGIOUS BUT IT WILL BE UNDERSTOOD AFTER PRACTICAL / DRY RUN EXAMPLE BY YOURSELF
+        "If There are N1 number of elements to the left of root element in Inorder array, then in the Preorder array after the root element the first N1 elements are left subtree nodes,
+        and if there are N2 number of elements to the right of the root element in the Inorder Array, then there are N2 number of right subtree nodes after the N1 number of left subtrees node in the Preorder Array"
 */
 
 struct BSTNode *root=NULL;   //Pointer that stores the memory address of the root node of the binary search tree (initially NULL)
@@ -44,31 +61,33 @@ struct BSTNode* insertNode(struct BSTNode* root,int data)      //Function that i
 
 struct BSTNode* constructTree(int* preOrderArray,int preOrderStart, int preOrderEnd,int *inOrderArray,int inOrderStart,int inOrderEnd)
 {
-    if(preOrderStart == preOrderEnd)
+
+    if(preOrderStart == preOrderEnd)        //if only one node remaining
         return createNode( preOrderArray[preOrderStart] );
     int rootIndex = -1;
-    for(int i=inOrderStart;i<=inOrderEnd;++i)
+    for(int i=inOrderStart;i<=inOrderEnd;++i)           //Loop to find the root node in the inorder array
     {
         if(inOrderArray[i] == preOrderArray[preOrderStart])
-        {
-            rootIndex = i;
+        {               
+            rootIndex = i;                      //if root node is found , store the index and break the loop
             break;
         }
     }
-    if(rootIndex != -1)
+    if(rootIndex != -1)                 //if root node found in the loop traversal
     {
-        struct BSTNode* root = createNode(inOrderArray[rootIndex]);
-        root->left = constructTree(preOrderArray,preOrderStart+1,preOrderStart+(rootIndex-inOrderStart), inOrderArray,inOrderStart,rootIndex-1);
-        root->right = constructTree(preOrderArray,preOrderStart+(rootIndex-inOrderStart)+1,preOrderEnd, inOrderArray,rootIndex+1,inOrderEnd);
+        struct BSTNode* root = createNode(inOrderArray[rootIndex]);             //create Binary Node for element at index 'rootIndex' at inorder traversal array
+        root->left = constructTree(preOrderArray,preOrderStart+1,preOrderStart+(rootIndex-inOrderStart), inOrderArray,inOrderStart,rootIndex-1);        //pointing the left subtree by recursion
+        root->right = constructTree(preOrderArray,preOrderStart+(rootIndex-inOrderStart)+1,preOrderEnd, inOrderArray,rootIndex+1,inOrderEnd);           //pointing the righ subtree by recursion
         
-        return root;
+        return root;            //finally return the root node to the caller function
     }
-    return NULL;
+    return NULL;            //if unexpected behaviour , return NULL
 
 }
 
 void inputPreorder_InorderArray(int** preOrderArray,int** inOrderArray,int n)
 {
+    //Function to create Dynamic Array of Preorder and Inorder by dereferencing the memory address of the pointer
     *preOrderArray=(int*)malloc(n*sizeof(int));
     *inOrderArray = (int*)malloc(n*sizeof(int));
     if(*preOrderArray == NULL || *inOrderArray == NULL)
@@ -76,6 +95,7 @@ void inputPreorder_InorderArray(int** preOrderArray,int** inOrderArray,int n)
         printf("Array Memory Allocation Failed . Please Try again \n");
         exit(1);
     }
+    //Inputing values for Preorder and Inorder Traversal Array
     for(int i=0;i<n;++i)
     {
         printf("Enter Preoder Node %d value : ",i);
@@ -103,18 +123,27 @@ int main()
     printf("Enter the length of Traversal Array : ");
     scanf("%d",&n);
 
-    int *preOrderArray=NULL,*inOrderArray = NULL;
-    inputPreorder_InorderArray(&preOrderArray,&inOrderArray,n);
-    struct BSTNode* root = constructTree(preOrderArray,0,n-1,inOrderArray,0,n-1);
+    int *preOrderArray=NULL,*inOrderArray = NULL;                       //pointers to point to the memory address of dynamic array
+    inputPreorder_InorderArray(&preOrderArray,&inOrderArray,n);         //function to create dynamic array of n elements with passing both pointer's address    
     
-    if(root==NULL)
+    struct BSTNode* root = constructTree(preOrderArray,0,n-1,inOrderArray,0,n-1);       //function to construct the Binary Tree and return the root node
+    
+    if(root==NULL)          //if TREE creation failed
     {
         printf("Couldn't Construct the Binary Tree, Verify the Preorder and Inorder Traversal Array \n");
+        free(preOrderArray);
+        free(inOrderArray);
+        free(root);
         exit(1);
     }
 
     printf("PreOrder Traversal of the Tree :  ");
-    preOrderTraversal(root);  
+    preOrderTraversal(root);            //printing preOrderTraversal of the Binary Node
+
+    //Dealloacting the dynamic memory after use
+    free(preOrderArray);
+    free(inOrderArray);
+    free(root);
     
     
 
