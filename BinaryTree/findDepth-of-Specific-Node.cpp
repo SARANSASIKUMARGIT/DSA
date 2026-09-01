920bc22322a7f67e0eb2b8921864efe29ccda0d2
Saran_SK <82465280+SARANSASIKUMARGIT@users.noreply.github.com>
2026-09-01 23:44:12 +0530

    Create findDepth-of-Specific-Node.cpp
    C++ Program to find the height of specific node in a Binary Tree by recursion

---
+++ BinaryTree/findDepth-of-Specific-Node.cpp
@@ -0,0 +1,111 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+                    /*      Saran SK       */
+
+/*
+    C++ Program to find the height of specific node in a Binary Tree by recursion
+*/
+
+struct BinaryNode                 //defining the structure of BinaryNode using struct 
+{
+    int data;                    //members of BinaryNode structure
+    BinaryNode* leftNode;
+    BinaryNode* rightNode;
+
+    BinaryNode(int value) : data(value) , leftNode(nullptr) , rightNode(nullptr) {};            //constructor method to initialize members of the object
+};
+
+int findDepthofNode(struct BinaryNode* root,int target)
+{
+    if(root == nullptr)
+        return -1;
+    if(root->data == target)
+        return 1;
+    int maxchildHeight = max(findDepthofNode(root->leftNode,target) , findDepthofNode(root->rightNode,target));
+    if(maxchildHeight == -1)
+        return -1;
+    return 1+maxchildHeight;
+}
+
+struct BinaryNode* insertNode(BinaryNode* root,int data)                        //Function to insert new node with value 'data' into the Binary Tree using BFS Traversal
+{
+    if(root == nullptr)                     //if tree is empty , then return create node with value 'data' and return as root
+        return new BinaryNode(data);
+
+    queue<BinaryNode*> q;                   //if tree is not empty, create a queue for BFS traversal of the tree to find the child that has 0 or 1 child by search in Level Order
+    q.push(root);                           //pushing the root node to the queues
+    while(!q.empty())
+    {
+        BinaryNode* frontNode = q.front();      
+        if(frontNode->leftNode == nullptr)                  // if current node has no left child then create new node and return it as the left child of the current node and break the loop
+        {
+            frontNode->leftNode = new BinaryNode(data);
+            break;
+        }
+        q.push(frontNode->leftNode);                    //if current node has left child push it to the queue
+        if(frontNode->rightNode == nullptr)                 // if current node has no right child then create new node and return it as the right child of the current node and break the loop
+        {
+            frontNode->rightNode = new BinaryNode(data);        
+            break;
+        }
+        q.push(frontNode->rightNode);                  //if current node has right child push it to the queue
+
+        q.pop();       //pop the front node from the queue as we already visited it
+    }
+    while(!q.empty())       //pop all the remaining nodes in the queue if exist
+        q.pop();
+    return root;        //return the root node
+}
+
+void inOrderTraversal(BinaryNode* root)
+{
+    if(root == nullptr)
+        return;
+    inOrderTraversal(root->leftNode);
+    cout<<root->data << " ";
+    inOrderTraversal(root->rightNode);
+}
+
+void deleteTree(BinaryNode* root)                           //function to deallocate all nodes of the tree from memory
+{
+    if(root == nullptr)
+        return;
+    deleteTree(root->leftNode);
+    deleteTree(root->rightNode);
+    delete root;
+}
+
+
+int main()
+{
+    struct BinaryNode* root = nullptr;
+
+    int n,num;
+    cout<<"Enter the number of nodes of the Binary Tree : ";
+
+    cin>>n;                             //inputing number of nodes
+    for(int i=0;i<n;++i)                                    
+    {
+        cout<<"Enter Node "<<i<<" value : ";
+        cin>>num;   
+        root=insertNode(root,num);              //inserting the data into the tree
+    }
+
+    inOrderTraversal(root);                                    //printing the nodes of the tree by inorder traversal
+
+    cout<<"Enter the node value to find depth : ";
+    cin>>num;
+    
+    int depth = findDepthofNode(root,num);
+    if(depth == -1)
+        cout<<"Node "<<num<< " not found \n";
+    else
+        cout<<"Depth of Node "<<num<<" is "<<depth<<endl;
+
+
+    deleteTree(root);                                         //deallocating the memory of all nodes of the return
+
+
+    return 0;
+}
