afa55a29e38d53b500335d9f11385252bd2e593b
Saran_SK <82465280+SARANSASIKUMARGIT@users.noreply.github.com>
2026-09-19 00:34:35 +0530

    Update breakLinkedList_and_LinkatEnd.cpp
    C++ Program to break a singly linked list into two linked list at a specific node 
        and link the first linked list into the end of the second linked list.
    
        TIME COMPLEXITY  : O(N) for traversing all the nodes of the linked list
        SPACE COMPLEXTIY : O(1) only constant extra space

--- LinkedList/breakLinkedList_and_LinkatEnd.cpp
+++ LinkedList/breakLinkedList_and_LinkatEnd.cpp
@@ -4,8 +4,8 @@ using namespace std;
                     /*      Saran SK       */
 
 /*
-    C++ Program to dynamically create nodes and construct Singly Linked List and
-    Reverse the Singly Linked List.
+    C++ Program to break a singly linked list into two linked list at a specific node 
+    and link the first linked list into the end of the second linked list.
 
     TIME COMPLEXITY  : O(N) for traversing all the nodes of the linked list
     SPACE COMPLEXTIY : O(1) only constant extra space
@@ -25,8 +25,30 @@ struct LinkedListNode* createLinkedListNode(int data=0)             //function t
     return newNode;
 }
 
-struct LinkedListNode* breakLinkedList_AndLinkAtEnd(struct LinkdedListNode* head)
-{
+struct LinkedListNode* breakLinkedList_AndLinkAtEnd(struct LinkedListNode* head,int key)  //function to break a singly linked list into two at a specific node 
+{                                                                                          //and link the first linked list into the end of the second linked list
+    struct LinkedListNode *current = head,*end= head,*prev=nullptr;
+    struct LinkedListNode* newHead = nullptr;
+
+    while(current)                          //traversing the nodes of the linked list until the end 
+    {
+        if(current->data == key)            //if current node is the target node
+        {
+            newHead = current;              //then the current node is the head of the linked list
+            if(prev)
+                prev->next = nullptr;       //breaking the linked list, by assigning the previous node's next reference to NULL
+            while(current->next)            //traversing to the last node of the list
+                current = current->next;
+        }
+        prev = current;                     //saving current node as previous and end node for every traversal
+        end = current;
+        current = current->next;            //moving to the next linked node
+    }
+
+    if(head == end || newHead == nullptr)       //if only single node or no target node is found, then return the default head node
+        return head;
+    end->next = head;                           //else assign the next reference of the end node to the head(head of first breaked linked list)
+    return newHead;                         //return the reference of 'newHead'
     
 }
 
@@ -67,7 +89,7 @@ void deleteLinkedList(LinkedListNode* headNode)
 
 int main()
 {
-    int n,num;
+    int n,num,target;
     struct LinkedListNode* headNode=nullptr;
     cout<<"Enter the number of nodes to add : ";
     cin>>n;
@@ -79,11 +101,14 @@ int main()
         headNode=appendNode(headNode,num);
     }
 
-    cout<<"Linked List Before Reversing : ";
+    cout<<"Enter the value of node at which the Linked List is to be Breaked : ";
+    cin>>target;
+
+    cout<<"Linked List Before Breaking into two and append the first linked list into the end : \n";
     printLinkedList(headNode);   
     
-    headNode = reverseLinkedList(headNode);                 //calling function to reverse the linked list that returns the reverse the linked list and returns the last node as the head node
-    cout<<"\nLinked List After Reversing : ";
+    headNode = breakLinkedList_AndLinkAtEnd(headNode,target);
+    cout<<"\nLinked List After Breaking into two and append the first linked list into the end : \n";
     printLinkedList(headNode);          
 
     deleteLinkedList(headNode);
