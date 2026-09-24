3e427d488ca213b9312e721a9e95ce055ab34c3b
Saran_SK <82465280+SARANSASIKUMARGIT@users.noreply.github.com>
2026-09-24 23:50:54 +0530

    Create printDiagonalNodes-Tree.cpp
    C++ Program to Print the Nodes in Diagonal Order in a Binary Tree
    
    
        TIME COMPLEXITY  : O(N) for traversing N number of nodes ,
        SPACE COMPLEXITY : O(N) where for storing values of N number of nodes in a result vector

---
+++ BinaryTree/printDiagonalNodes-Tree.cpp
@@ -0,0 +1,117 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+                    /*      Saran SK       */
+
+/*
+    C++ Program to Print the Nodes in Diagonal Order in a Binary Tree
+
+
+    TIME COMPLEXITY  : O(N) for traversing N number of nodes ,
+    SPACE COMPLEXITY : O(N) where for storing values of N number of nodes in a result vector
+*/
+
+struct BinaryNode                 //defining the structure of BinaryNode using struct 
+{
+    int data;                    //members of BinaryNode structure
+    BinaryNode* leftNode;
+    BinaryNode* rightNode;
+
+    BinaryNode(int value) : data(value) , leftNode(nullptr) , rightNode(nullptr) {};            //constructor method to initialize members of the object
+};
+
+struct BinaryNode* insertNode(BinaryNode* root,int data)                        //Function to insert new node with value 'data' into the Binary Tree using BFS Traversal
+{
+    if(root == nullptr)                     //if tree is empty , then return create node with value 'data' and return as root
+        return new BinaryNode(data);
+
+    queue<BinaryNode*> q;                   //if tree is not empty, create a queue for BFS traversal of the tree to find the child that has 0 or 1 child by search in Level Order
+    q.push(root);                           //pushing the root node to the queues
+    while(!q.empty())
+    {
+        BinaryNode* frontNode = q.front();      
+        if(frontNode->leftNode == nullptr)                  // if current node has no left child then create new node and return it as the left child of the current node and break the loop
+        {
+            frontNode->leftNode = new BinaryNode(data);
+            break;
+        }
+        q.push(frontNode->leftNode);                    //if current node has left child push it to the queue
+        if(frontNode->rightNode == nullptr)                 // if current node has no right child then create new node and return it as the right child of the current node and break the loop
+        {
+            frontNode->rightNode = new BinaryNode(data);        
+            break;
+        }
+        q.push(frontNode->rightNode);                  //if current node has right child push it to the queue
+
+        q.pop();       //pop the front node from the queue as we already visited it
+    }
+    while(!q.empty())       //pop all the remaining nodes in the queue if exist
+        q.pop();
+    return root;        //return the root node
+}
+
+void deleteTree(BinaryNode* root)                           //function to deallocate all nodes of the tree from memory
+{
+    if(root == nullptr)
+        return;
+    deleteTree(root->leftNode);
+    deleteTree(root->rightNode);
+    delete root;
+}
+
+void printDiagonalNodes(struct BinaryNode* root)         //function to print the nodes in diagonal order of the binary tree
+{
+    if(root == nullptr)                 //return if tree is empty
+        return;
+    queue<struct BinaryNode*> q;
+    q.push(root);
+    vector<int> res;
+
+    while(!q.empty())
+    {
+        int size = q.size();                //calculating the number of current diagonal nodes 
+        for(int i=1;i<=size;++i)
+        {
+            struct BinaryNode* currentNode = q.front();             //fetching current front node (first node of current diagonal) 
+            q.pop();                                                //pop the node
+            while(currentNode)
+            {
+                res.push(currentNode->data);                    //push the current node value to the result vector
+                if(currentNode->leftNode)                       //if nodes has left child then it should be pushed to the queue for traversing it for the next diagonal
+                    q.push(currentNode->leftNode);
+                currentNode = currentNode->rightNode;           //move to right child of current node
+            }
+        }
+    }
+
+    cout<<"Elements Visited in Diagonal Order : ";
+    for(int a : res)
+        cout<<a<<" ";
+    cout<<endl;
+    
+}
+
+
+
+int main()
+{
+    struct BinaryNode* root = nullptr;
+
+    int n,num;
+    cout<<"Enter the number of nodes of the Binary Tree : ";
+
+    cin>>n;                             //inputing number of nodes
+    for(int i=0;i<n;++i)                                    
+    {
+        cout<<"Enter Node "<<i<<" value : ";
+        cin>>num;   
+        root=insertNode(root,num);              //inserting the data into the tree
+    }
+
+    printDiagonalNodes(root);
+
+    deleteTree(root);                                         //deallocating the memory of all nodes of the return
+
+
+    return 0;
+}
